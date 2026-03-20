e8c9fa35e2369c69495fdd9e699dbd3a11b5a59d
Guilherme Izidio Nogueira <guilherme.elinogueira@gmail.com>
2026-03-19 22:55:47 -0300

    PHP
    That’s already better than Marcelo!

---
+++ PHP-Basico/Aula01/ola.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primeiro exemplo PHP do curso PHP</title>
+    <style>
+        h2 {
+            color: #bf8989;
+            text-shadow: 1px 1px 1px white;
+        }
+    </style>
+</head>
+<body>
+    <h1>Testando o PHP</h1>
+    <?php
+        echo "<h2>Ola, <br/> Mundo</h2>";
+    ?>
+
+</body>
+</html>
