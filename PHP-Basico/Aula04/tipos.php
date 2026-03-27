0132a99d348eb82965ea115873996ae9ae95f2ee
Guilherme Izidio Nogueira <guilherme.elinogueira@gmail.com>
2026-03-26 21:47:06 -0300

    PHP
    With Gustavo Guanabara’s help, I am learning the basics of PHP to improve my programming skills

---
+++ PHP-Basico/Aula04/tipos.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="_css/estilo.css">
+</head>
+<body>
+<div>
+    <?php
+    $n = 4;
+    $no = (string)"Gustavo\n";
+    echo $no;
+
+    $nome = "Guilherme";
+    $idade = 17;
+    echo $nome." tem ".$idade." anos";
+    echo "$nome tem $idade anos";
+
+    ?>
+</div>
+</body>
+</html>
