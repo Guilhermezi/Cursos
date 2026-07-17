782653dc937d6cfbc587910da52ab74828998e47
guilhermezi <guilherme.elinogueira@gmail.com>
2026-07-17 18:04:46 -0300

    Operadores JAVA

---
+++ MaratonaJavaViradoNoJiraya/maratona-java/src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -0,0 +1,29 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula04Operadores {
+    static void main(String[] args) {
+        // + - / *
+        int number1 = 10;
+        int number2 = 20;
+        double result = number2 / (double) number1;
+        System.out.println("Valor:"+result);
+
+        // %
+        int resto = 21 % 7;
+        System.out.println(resto);
+
+        // < > <= >= == !=
+
+        boolean isDezMaiorQueVinte = 10 > 20;
+        boolean isDezMenorQueVinte = 10 < 20;
+        boolean isDezIgualVinte = 10 == 20;
+        boolean isDezIgualDez = 10 == 10;
+        boolean isDezDiferenteDez = 10 != 10;
+        System.out.println("isDezMaiorQueVinte "+isDezMaiorQueVinte);
+        System.out.println("isDezMenorQueVinte "+isDezMaiorQueVinte);
+        System.out.println("isDezIgualVinte "+isDezIgualVinte);
+        System.out.println("isDezIgualDez "+isDezIgualDez);
+        System.out.println("isDezDiferenteDez "+isDezDiferenteDez);
+
+    }
+}
