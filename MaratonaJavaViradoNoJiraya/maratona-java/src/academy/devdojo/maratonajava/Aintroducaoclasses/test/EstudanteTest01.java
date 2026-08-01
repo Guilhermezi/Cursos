162da097b650c8cefd660b721533c65f298f7d7f
guilhermezi <guilherme.elinogueira@gmail.com>
2026-07-31 22:13:18 -0300

    Já chego em orientado a Objeto
     já foi visto mas mesmo assim é bom rever já que isso é core da programação

---
+++ MaratonaJavaViradoNoJiraya/maratona-java/src/academy/devdojo/maratonajava/Aintroducaoclasses/test/EstudanteTest01.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.javacore.Aintroducaoclasses.test;
+
+import academy.devdojo.maratonajava.javacore.Aintroducaoclasses.dominio.Estudante;
+
+public class EstudanteTest01 {
+    static void main(String[] args) {
+        Estudante estudante = new Estudante();
+        estudante.nome = "Lufft";
+        estudante.age = 18;
+        estudante.sexo = 'M';
+
+        System.out.println(estudante.nome);
+        System.out.println(estudante.age);
+        System.out.println(estudante.sexo);
+    }
+}
