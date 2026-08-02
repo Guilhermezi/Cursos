654eb27d32a97c050c0326cedcaf96dd1be08ef3
guilhermezi <guilherme.elinogueira@gmail.com>
2026-08-01 23:47:11 -0300

    Na verdade, consegui dar conta rapidamente de muitas coisas simples

--- MaratonaJavaViradoNoJiraya/maratona-java/src/maratonajava/introducao/Aula08ArraysMultidimensionais01.java
+++ MaratonaJavaViradoNoJiraya/maratona-java/src/maratonajava/introducao/Aula08ArraysMultidimensionais01.java
@@ -2,15 +2,15 @@ package academy.devdojo.maratonajava.introducao;
 
 public class Aula08ArraysMultidimensionais01 {
     static void main(String[] args) {
-            int [][] dias = new int [3][3];
+        int[][] dias = new int[3][3];
 
-            dias [0][0] = 31;
-            dias [0][1] = 28;
-            dias [0][2] = 31;
+        dias[0][0] = 31;
+        dias[0][1] = 28;
+        dias[0][2] = 31;
 
-            dias [1][0] = 31;
-            dias [1][1] = 28;
-            dias [1][2] = 31;
+        dias[1][0] = 31;
+        dias[1][1] = 28;
+        dias[1][2] = 31;
 
         for (int i = 0; i < dias.length; i++) {
             for (int j = 0; j < dias[i].length; j++) {
@@ -23,6 +23,7 @@ public class Aula08ArraysMultidimensionais01 {
                 System.out.println();
             }
         }
-        }
+
     }
 }
+
---
+++ MaratonaJavaViradoNoJiraya/maratona-java/src/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTest02.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.test;
+
+import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Calculadora;
+
+public class CalculadoraTest02 {
+    static void main(String[] args) {
+        Calculadora calc = new Calculadora();
+        System.out.println("multiplicaDoisNumeros");
+        calc.multiplicaDoisNumeros(8, 4);
+    }
+}
