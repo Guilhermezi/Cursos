b30d00b8f32d495aeab20e0ed3fe9c6640b6f34a
guilhermezi <guilherme.elinogueira@gmail.com>
2026-08-06 17:42:14 -0300

    Bastante coisa até

--- MaratonaJavaViradoNoJiraya/maratona-java/src/maratonajava/javacore/Bintroducaometodos/dominio/Estudante.java
+++ MaratonaJavaViradoNoJiraya/maratona-java/src/maratonajava/javacore/Bintroducaometodos/dominio/Estudante.java
@@ -4,4 +4,11 @@ public class Estudante {
     public String nome;
     public int age;
     public char sexo;
+
+    public void imprime(){
+        System.out.println("----------------");
+        System.out.println(this.nome);
+        System.out.println(this.age);
+        System.out.println(this.sexo);
+    }
 }
--- MaratonaJavaViradoNoJiraya/maratona-java/src/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTest05.java
+++ MaratonaJavaViradoNoJiraya/maratona-java/src/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTest05.java
@@ -2,11 +2,11 @@ package academy.devdojo.maratonajava.javacore.Bintroducaometodos.test;
 
 import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Calculadora;
 
-public class CalculadoraTest02 {
-    static void main(String[] args) {
+public class CalculadoraTest05 {
+    static void main(String... args) {
         Calculadora calc = new Calculadora();
-
-        System.out.println("multiplicaDoisNumeros");
-        calc.multiplicaDoisNumeros(8, 4);
+        int[] numeros = {1, 2, 3, 5, 6, 8, 9};
+        calc.somaArray(numeros);
+        calc.somaVarArgs(1, 2, 3, 5, 6, 5, 4, 6, 8);
     }
 }
